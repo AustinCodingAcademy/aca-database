cce7faf9920686224c0f1830f1b2902d433c3bb6
Jon Woo <jw56578@gmail.com>
2019-02-04 16:14:39 -0600

    remove state

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 let fs = require("fs");
 
 exports.create = function(type,data,doneCallback){
-
+  
 }
 exports.find = function(type,id,doneCallback){
 
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
 var http = require('http');
-let users = require("./state").users;
-let products = require("./state").products;
 let server = http.createServer(messageReceived);
 server.listen(8080);
 
@@ -10,11 +8,12 @@ function messageReceived(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
     if(req.method === "GET" && req.url === "/users"){
-      getUsers(req, res);
+      res.end();
     }
     else if(req.method === "GET" && req.url.indexOf("/users/") > -1){
       let id = req.url.split("/");
-      
+      id = Number(id[2]);
+      res.end();
     }
     else if(req.method === "POST" && req.url === "/users"){
         let body = [];
@@ -24,16 +23,14 @@ function messageReceived(req, res) {
           body = Buffer.concat(body).toString();
           let user = JSON.parse(body);
           user._id = users.length + 1;
-          users.push(user);
+         
+          res.end();
         });
     }
     else{
      res.write("Not Found");
+     res.end();
     }
-    res.end();
+    
 }
 
-function getUsers(req, res){
-    let usersJSON = JSON.stringify(users);
-    res.write(usersJSON);
-}
